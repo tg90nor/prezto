9b0638836222d246779f1e55f2ebf950d9160251
Thomas Geirhovd <tg90nor@gmail.com>
2017-02-21 02:49:35 +0100

    Change LS colors for completion on bsd systems to better match linux

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -82,7 +82,7 @@ else
     export LSCOLORS='exfxcxdxbxGxDxabagacad'
 
     # Define colors for the completion system.
-    export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=36;01:cd=33;01:su=31;40;07:sg=36;40;07:tw=32;40;07:ow=33;40;07:'
+    export LS_COLORS='di=01;34:ln=35:so=32:pi=33:ex=01;32:bd=36;01:cd=33;01:su=31;40;07:sg=36;40;07:tw=32;40;07:ow=33;40;07:'
 
     alias ls="${aliases[ls]:-ls} -G"
   else
